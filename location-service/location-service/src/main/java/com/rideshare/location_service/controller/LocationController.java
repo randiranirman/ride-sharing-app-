80c2937d3d6fcb3e6c3c230a6261935db82cad95
randiranirman <randiranirman20011210@gmail.com>
2026-04-17 21:47:23 +0530

    added  controller methods

--- location-service/location-service/src/main/java/com/rideshare/location_service/controller/LocationController.java
+++ location-service/location-service/src/main/java/com/rideshare/location_service/controller/LocationController.java
@@ -1,11 +1,15 @@
 package com.rideshare.location_service.controller;
 
 
+import com.rideshare.location_service.dto.DriverLocationRequest;
+import com.rideshare.location_service.dto.NearByDriverResponse;
 import com.rideshare.location_service.service.LocationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping( "/api/v1/locations")
@@ -15,6 +19,51 @@ import org.springframework.web.bind.annotation.RestController;
 public class LocationController {
 
     private final LocationService locationService ;
+
+    // driver phone calls every 3 seconds
+
+    @PostMapping("/drivers/update")
+    public ResponseEntity<String>  updateDriverLocation(@RequestBody DriverLocationRequest request) {
+
+
+
+        locationService.updateDriverLocation(request)
+;
+
+        return ResponseEntity.ok("Driver Location  Upadated");
+
+
+
+
+    }
+
+
+
+
+    // matching service calls this when ride is requested
+    @GetMapping("/drivers/nearby")
+    public List<NearByDriverResponse> getNearByDrivers(@RequestParam double latitude,@RequestParam
+                                                       double longitude , @RequestParam( defaultValue = "5.0" ) double radius) {
+
+
+
+
+        return LocationService.findNearByDrivers( latitude, longitude, radius);
+
+
+
+    }
+
+    // when driver goes  offline
+
+    @DeleteMapping("/drivers/{driverId}")
+    public String removeDriverByDriverId( @PathVariable String driverId ) {
+
+
+        locationService.removeDriver( driverId);
+
+        return " driver removed ";
+    }
 
 
 
