ce26e6ba0afc79fb18eca07b9b05fc59a49b4f1a
randiranirman <randiranirman20011210@gmail.com>
2026-04-18 15:45:17 +0530

    added the model and dto

---
+++ ride-service/ride-service/src/main/java/com/rideshare/ride_service/model/RideStatus.java
@@ -0,0 +1,12 @@
+package com.rideshare.ride_service.model;
+
+public enum RideStatus {
+
+    REQUESTED,
+    MATCHING,
+    ACCEPTED,
+    DRIVER_ARRIVING,
+    RIDE_STARTED ,
+    COMPLETED ,
+    CANCELLED
+}
---
+++ ride-service/ride-service/src/main/java/com/rideshare/ride_service/model/Rider.java
@@ -0,0 +1,82 @@
+package com.rideshare.ride_service.model;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@Table ( name = "riders")
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Rider {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private String id ;
+
+
+
+
+    @Column( nullable = false)
+
+    private String riderId ;
+
+    @Column( nullable = false)
+    private  String driverId ;
+
+    @Column( nullable = false)
+
+    private double pickUpLatitude;
+    @Column( nullable = false)
+
+
+    private double pickUpLongitude ;
+    @Column( nullable = false)
+
+    private String pickUpAddress ;
+    @Column( nullable = false)
+
+    private double dropLatitude ;
+    @Column( nullable = false)
+
+      private double dropLongitude ;
+    @Column( nullable = false)
+
+      private String dropAddress ;
+
+
+    @Enumerated( EnumType.STRING)
+    @Column( nullable = false)
+
+      private RideStatus status;
+
+    @Column( nullable = false)
+
+      private double estimatedFare ;
+    @Column( nullable = false)
+
+      private double actualFare ;
+
+
+    @CreationTimestamp
+      private LocalDateTime cratedAt ;
+
+    @UpdateTimestamp
+      private LocalDateTime updateAt ;
+      private  LocalDateTime staredAt ;
+      private LocalDateTime completedAt  ;
+
+
+
+
+
+}
