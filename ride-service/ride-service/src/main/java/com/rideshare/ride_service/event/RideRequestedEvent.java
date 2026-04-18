d66cb520befb9772f5a97e3fdb3503eb67469df9
randiranirman <randiranirman20011210@gmail.com>
2026-04-18 18:41:35 +0530

    finished the request ride method in rideService

--- ride-service/ride-service/src/main/java/com/rideshare/ride_service/event/RideRequestedEvent.java
+++ ride-service/ride-service/src/main/java/com/rideshare/ride_service/event/RideRequestedEvent.java
@@ -1,4 +1,28 @@
 package com.rideshare.ride_service.event;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RideRequestedEvent {
+
+    private String rideId;
+    private String riderId;
+    double pickUpLatitude;
+
+
+    double pickUpLongitude;
+
+    String pickUpAddress;
+
+    double dropLatitude;
+
+    double dropLongitude;
+
+    String dropAddress;
 }
+
+
