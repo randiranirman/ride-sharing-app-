ce1980615afec56d0578530f783ae5118faafd6b
randiranirman <randiranirman20011210@gmail.com>
2026-04-19 00:39:09 +0530

    created the  kafka topic builders

--- ride-service/ride-service/src/main/java/com/rideshare/ride_service/config/KafkaConfig.java
+++ ride-service/ride-service/src/main/java/com/rideshare/ride_service/config/KafkaConfig.java
@@ -1,5 +1,38 @@
 package com.rideshare.ride_service.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.internals.Topic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaConfig {
+
+    // topic where ride service publshes ride request
+    // matching service subcribes to the topic
+
+    @Bean
+    public NewTopic rideRequestedTopic () {
+
+
+        return TopicBuilder.name("ride.requested").partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+
+    // topic where matchin service published match result
+
+
+
+    @Bean
+    public NewTopic rideMatchedTopic() {
+
+        return TopicBuilder.name("ride.matched")
+                .partitions(3)
+                .replicas(1).build();
+    }
+
 
 }
