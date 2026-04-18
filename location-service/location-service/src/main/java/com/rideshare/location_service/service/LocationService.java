99ab182ef8c233aea1c9496158cacf31903486a5
randiranirman <randiranirman20011210@gmail.com>
2026-04-18 14:59:59 +0530

    updated the location service

--- location-service/location-service/src/main/java/com/rideshare/location_service/service/LocationService.java
+++ location-service/location-service/src/main/java/com/rideshare/location_service/service/LocationService.java
@@ -5,11 +5,15 @@ import com.rideshare.location_service.dto.DriverLocationRequest;
 import com.rideshare.location_service.dto.NearByDriverResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.geo.*;
 import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.RedisTemplate;
+
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,7 +28,58 @@ public class LocationService {
 
     public  List<NearByDriverResponse> findNearByDrivers(double latitude, double longitude, double radius) {
 
-        // find near by drivers
+        // find nearby drivers
+      // called by matching service on ride request
+         //  maps to redis  GEORADIUS command
+
+
+        log.info("finding drivers near lat : {} long: {} within radius : {} " , latitude, longitude, radius);
+
+
+        // created the search area
+        Circle searchArea=  new Circle( new Point(longitude, latitude) , new Distance(
+
+                radius, Metrics.KILOMETERS
+        ));
+
+
+         GeoResults<RedisGeoCommands.GeoLocation<String>>   results = redisTemplate
+                 .opsForGeo().radius(
+                         DRIVER_GEO_KEY ,
+                         searchArea,
+                         RedisGeoCommands.GeoRadiusCommandArgs
+                                 .newGeoRadiusArgs()
+                                 .includeDistance()
+                                 .sortAscending()
+                                 .limit(10)
+                 );
+
+         List<NearByDriverResponse> nearByDrivers = new ArrayList<>();
+
+         if( results != null) {
+
+             results.getContent().forEach(
+
+                     result -> {
+                         RedisGeoCommands.GeoLocation<String> location= result.getContent();
+
+                         nearByDrivers.add( new NearByDriverResponse(
+
+                                 location.getName(),
+                                 location.getPoint().getX(),
+                                 location.getPoint().getY(),
+                                 result.getDistance().getValue()
+                         ));
+                     }
+             );
+         }
+
+
+         return nearByDrivers;
+
+
+
+
 
 
 
@@ -35,7 +90,7 @@ public class LocationService {
     /** update driver location in redis
      * called every 3 seconds  by driveres phone
      *
-     * @param request
+     *
      */
     public void updateDriverLocation(DriverLocationRequest request) {
 
@@ -68,6 +123,13 @@ public class LocationService {
     }
 
     public void removeDriver(String driverId) {
+
+        log.info("removing driver ");
+        redisTemplate.opsForGeo().remove(DRIVER_GEO_KEY ,  driverId
+        );
+
+
+
 
 
 
